29a784f19aecb3e673b5df5659e6599ab52edd90
LZ <laraezegart@gmail.com>
2021-02-16 20:50:37 -0800

    created models folder and added schema file to it

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    
+  });
 
 app.listen(PORT, () => {
     console.log(`App is running at http://localhost:${PORT}`);
