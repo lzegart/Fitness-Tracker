f6a9555ad95259e74459471e108e89707b91ce5b
LZ <laraezegart@gmail.com>
2021-02-22 15:33:28 -0800

    deploying

--- server.js
+++ server.js
@@ -12,10 +12,14 @@ app.use(express.json());
 app.use(express.static('./public'));
 app.use(logger('dev'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/sheltered-cove-87815", { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
-  });
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 require('./routes/html-routes')(app);
 require('./routes/api-routes')(app);
